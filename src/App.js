f1cd926025f64eff0a514a803930c3e6ab5216d3
weeraphat425 <5910110425@psu.ac.th>
2018-09-17 10:47:15 +0700

    import CharacterCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello{"World"}
+      <div>
+        <CharacterCard value="h"/>
+        <CharacterCard value="i"/>
       </div>
     );
   }
