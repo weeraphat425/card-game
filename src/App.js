bc0d00e1f42bdc42e8ed51cfae349f27249e6fcb
weeraphat425 <5910110425@psu.ac.th>
2018-09-17 10:49:38 +0700

    Component map

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
-
+const word = "Hello";
 class App extends Component {
   render() {
     return (
-      <div>
-        <CharacterCard value="h"/>
-        <CharacterCard value="i"/>
+      <div>{
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      }
       </div>
     );
   }
