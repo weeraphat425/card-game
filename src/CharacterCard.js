89b56a0e8745ae83b273c7593f5e1b5539a7c834
weeraphat425 <5910110425@psu.ac.th>
2018-09-17 11:21:49 +0700

    States

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
-export default class CharacterCard extends Component {
+export default class CharacterCard extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            active: false,
+        }
+    }
+
+    activate = () => {
+        if(this.state.active){
+            this.setState({active: true})
+        }
+    }
+
     render(){
-        return (
-            <div>{this.props.value}</div>
+        let className = `card ${this.state.active ? 'activeCard': ''}`
+        return(
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
         )
     }
-}
+  }
